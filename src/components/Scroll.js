4d10d3616d37db880d3f191082f6ea26ace54202
sufyanali <sufyanalimir313@gmail.com>
2022-03-14 12:41:07 +0530

    finishing proj

---
+++ src/components/Scroll.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "1px solid #000",
+        height: "600px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
